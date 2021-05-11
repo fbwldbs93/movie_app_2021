78f028d5acc43913287f199e5a879c4068124d28
fbwldbs93 <fbwldbs93@naver.com>
2021-05-12 01:30:52 +0900

    jsx_props_state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,85 @@
 import React from "react";
 //import Potato from "./Potato";
+import PropTypes from "prop-types";
 
+/*
+const foodILike = [
+  {
+    id:1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg", 
+    rating:5
+  },
+  {
+    id:2, 
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg", 
+    rating:4.9
+  },
+  {
+    id:3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb", 
+    rating:4.8
+  },
+  {
+    id:4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg", 
+    rating:5.5
+  },
+  {
+    id:5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating:4.7
+  }
+];*/
 
-function Food({fav}){
+/*
+  food를 이름과 함께 렌더링 하기!
+  :: javacript 함수를 사용할 것임 (map!!)
+
+  map : array로 부터 너에게 array를 줌.
+  - 어떤 array의 각 item에서 function을 실행하는 array를 가지는 함수
+  - 그 function의 result를 갖는 array를 나에게 줌.
+
+  ---------------- 콘솔로그 실험 -----------
+
+  const friends = ["dal", "mark", "lynn", "japan guy"];
+
+  friends 
+  ====>  (4) ["dal", "mark", "lynn", "japan guy"]
+
+  friends.map(current => {
+    console.log(current);
+    return 0;
+  });
+  ====>  dal, mark , lynn, japan guy  / (4) [0,0,0,0]
+  
+
+  ※ map 은 function을 취해서 그 function을 array의 각 item에 적용한다.
+  ※ map 은 array의 각 item에 function을 적용하고 array를 줌.
+  ※ map 은 array를 취하고 우리가 정확히 원하는 array를 반환함.
+*/
+
+/*
+function renderFood(dish){
+  console.log(dish); // foodILike의 오브젝트 array들 출력됨.
+  return <Food name={dish.name} picture={dish.image}/>
+}
+*/
+
+function Food({name, picture, rating}){
 
   /*
-  console.log(props);
-  //fav="kimchi" 출력됨
-  console.log(props.fav);
-  //kimchi 출력됨 
+  console.log(props);   //fav="kimchi" 출력됨
+  console.log(props.fav);   //kimchi 출력됨 
   */
 
   /*
@@ -16,22 +87,72 @@ function Food({fav}){
     둘다 kimchi 출력됨.
   */
 
-  return <h1>I like {fav}</h1>;
+  return <div>
+    <h2>I Like {name}</h2>
+    <h4>{rating}/5</h4>
+    <img src={picture} alt={name}/>
+  </div>;
+}
+
+Food.propTypes ={
+  //내가 얻고 싶은 props에 대한 설명을 적을거임
+  name : PropTypes.string.isRequired,
+  picture : PropTypes.string.isRequired,
+  rating : PropTypes.number
+  /*
+    isRequired 는 필수라는 의미.
+    위와 같이 isRequired를 빼면, rating을 빼먹어도 오류가 나지 않음.
+    다만, rating이 number가 아닐때는 오류발생함.
+  */
 }
 
 
+/*
 function App() {
   return (
     <div>
-      <h1>Hello!!!!</h1>
-      <Food fav="kimchi"/>
-      <Food fav="ramen"/>
-      <Food fav="samgiopsal"/>
-      <Food fav="chukumi"/>
+     {foodILike.map(dish => (
+        <Food 
+          key = {dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating = {dish.rating}
+          />
+     ))}
+     
     </div>
   );
+  
+}*/
 
-  /* ----- jsx + Props 총정리 ---------
+/*※ 위의 dish는 object 임.*/
+
+/* 
+  ::: key props는 <Food/>로 전달되지 않음! <Food/>가 사용하고 있지 않기 때문 
+  - 기본적으로 react 내부에서 사용하기 위한 것...!!
+*/
+/* ------------------
+  ※※※ 에러발생!!  : 각각 list내의 child는 unique한 key prop를 가져야함.
+
+  - 즉, 모든 react 의 element들은 유일해야함
+  - 이것들을 list안으로 집어넣을때, 이것들은 유일성을 잃어버리게 됨.
+
+------------------*/
+
+/*{foodILike.map(renderFood)}*/
+     
+
+
+/* ---------- prop-types 설치
+- prop-types 
+ :: 내가 전달받은 props가 내가 원하는 props인지 확인해줌
+ - 왜냐면...우리는 종종 실수를 하기 때문.....
+
+*/
+
+
+
+/* ----- jsx + Props 총정리 ---------
     0. jsx는 HTML + Javascript 임. 
     1. 컴포넌트는 대문자로 시작해야함.
     2. father(App) 가 children(Food)에게 data 보내려면? Props 사용!
@@ -41,7 +162,7 @@ function App() {
   */
 
 
-  //---- 첫 리액트 개념!
+  /*---- 첫 리액트 개념! */
   /*
     1. food 컴포넌트에 "kimchi"라는 value를 가진 property(속성) "name"을 줌.
     2. food 컴포넌트에 "fav"라는 이름의 property 로 바꿔보기. 
@@ -113,7 +234,7 @@ function App() {
   */
 
 
-  //-------------------JSX and PROPS 강의 ---------------------
+  /*-------------------JSX and PROPS 강의 ---------------------*/
 
    /*
     :: 우리는 function App(){}을 가지고 있고
@@ -129,8 +250,57 @@ function App() {
   
   */
 
+  /*================================================================================================================================================================================================= */
+  /*------------------ State ----------------- */
 
+/*
+    state는 보통 우리가 동적 데이터와 함께 작업할 때 만들어짐.
+    변하는 데이터, 존재하지 않는 데이터임
+*/
+
+class App extends React.Component{
+  state = {
+    count : 0
+  };
+
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+
+  render(){
+      return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+      );
+  }
 }
+
+/*-------  클래스형 함수 --------
+  - react Component 는 많은 것을 가지고 있음.
+  그 중 하나가 state 임.
+  -  매번 우리가 component 를 만들 때마다 모든 것을 다
+  구현하고 싶지 않음. 그래서 extends 를 한 것.
+
+--------------------------------------------
+클래스형 함수는 class임. 
+:: react component로 부터 확장되고 screen에 표시됨.
+- react는 자동적으로 모든 클래스형 함수의 render method를 실행하고자 함...!!! 자동적으로!
+  
+*/
+
+/* --------------- State --------------
+  state 는 object 이다.
+  - component의 data를 넣을 공간이 있고. 이 데이터는 변함.
+*/
+
+
+
 
 export default App;
 
